b4b1e027fcd0ef36b4c6a3768df887cdaa437a7e
Arsh <80232792+GoodDeveloper2018@users.noreply.github.com>
2024-12-07 17:05:01 -0500

    Fix AdventDay2.cpp
    Fix AdventOfCode Day 2 solution by correctly handling both increasing and decreasing sequences with valid differences between -3 and -1 as well as 1 and 3. Replaced usage of scanf and cin with cin and stringstream parsing to prevent potential scanning issues. Added checks for empty lines and inputs.

--- AdventDay2.cpp
+++ AdventDay2.cpp
@@ -1,34 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main () {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-	int lines = 1000;
-	int solution = 0;
-	for (int i = 0; i < lines; i++) {
-		vector<int> input;
-		while (true) {
-			int x;
-			scanf("%d", &x);
-			input.push_back(x);
-			char c;
-			scanf("%c", &c);
-			if (c == "\n") {
-				break;
-			}
-		}
-	}
+    int lines = 1000;
+    int solution = 0;
 
-	int k = input.size();
-	cout << k << endl;
-	bool only_increment;
-	bool only_decrement;
-    for (int j = 0; j < k; j++) {
-        int diff = input[i+1] - input[i];
-		int add = input[i+1] + input[i];
+    for (int i = 0; i < lines; i++) {
+        vector<int> input;
+        string line;
+        if (!getline(cin, line)) break; 
+        if (line.empty()) continue; 
+
+        int x;
+        stringstream ss(line);
+        while (ss >> x) {
+            input.push_back(x);
+        }
+
+        if (input.empty()) continue;
+        
+        bool increasing = true, decreasing = true;
+        for (size_t j = 1; j < input.size(); ++j) {
+            int diff = input[j] - input[j - 1];
+            
+            if (!((diff >= 1 && diff <= 3) || (diff <= -1 && diff >= -3))) {
+                increasing = decreasing = false;
+                break;
+            }
+
+            if (input[j] > input[j - 1]) decreasing = false;
+            if (input[j] < input[j - 1]) increasing = false;
+        }
+
+        if (increasing || decreasing) {
+            solution++;
+        }
     }
 
-	return 0;
+    cout << solution << "\n";
+    return 0;
 }
