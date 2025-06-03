d8aa01e662b0118e405e05b03becd518b62df4d2
Arsh <80232792+GoodDeveloper2018@users.noreply.github.com>
2025-06-03 03:11:49 -0400

    Update AdventDay3.cpp
    2nd part added with do, don't regex search/implementation

--- AdventDay3.cpp
+++ AdventDay3.cpp
@@ -1,40 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    // Read entire input
+void partOne() {
     ostringstream oss;
     oss << cin.rdbuf();
     string input = oss.str();
 
     // Regex to match mul(X,Y) with X and Y as 1-3 digit numbers
     regex pattern(R"(mul\(([0-9]{1,3}),([0-9]{1,3})\))");
-    smatch match;
-
+    
     long long total_sum = 0;
-
-    // Search for all valid mul instructions
-    string::const_iterator searchStart(input.cbegin());
-    while (regex_search(searchStart, input.cend(), match, pattern)) {
-        string num1_str = match[1].str();
-        string num2_str = match[2].str();
-
-        // Validate digits (optional, since regex ensures digits)
-        bool valid1 = all_of(num1_str.begin(), num1_str.end(), ::isdigit);
-        bool valid2 = all_of(num2_str.begin(), num2_str.end(), ::isdigit);
-
-        if (valid1 && valid2) {
-            int x = stoi(num1_str);
-            int y = stoi(num2_str);
-            total_sum += (long long)x * (long long)y;
-        }
-
-        searchStart = match.suffix().first;
+	smatch match;
+	auto it = input.cbegin();
+	
+    while (regex_search(it, input.cend(), match, pattern)) {
+		long long a = stoll(match[1]);
+		long long b = stoll(match[2]);
+		total_sum += a*b;
+		it = match.suffix().first;
     }
+    cout << total_sum << endl;
+}
 
-    cout << total_sum << "\n";
+void partTwo() {
+	ostringstream oss;
+    oss << cin.rdbuf();
+    string input = oss.str(); 
+    
+    regex pattern2(R"(\b(do\(\)|don't\(\)|mul\(([0-9]{1,3}),([0-9]{1,3})\)))");
+    bool enabled = true;
+    long long total = 0;
+    smatch m;
+    auto it = input.cbegin();
+    
+    while (regex_search(it, input.cend(), m, pattern2)) {
+    	string t = m[0];
+    	if (t == "do()") {
+    		enabled = true;
+    	}
+    	else if (t == "don't()") {
+    		enabled = false;
+    	}
+    	else if (enabled) {
+    		long long a = stoll(m[2]);
+    		long long b = stoll(m[3]);
+    		total += a*b;
+    	}
+    	it = m.suffix().first;
+    }
+    cout << total << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+	//partOne();
+	partTwo();
+	
     return 0;
 }
